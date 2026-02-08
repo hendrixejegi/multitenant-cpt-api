5c09d7ab49484e210750d3cb5251d7bc307b57ff
Hendrix Ejegi <tosanwumiae@gmail.com>
2026-02-09 00:31:41 +0100

    include error handler in app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,21 @@
 import config from './config';
-import express from 'express';
+import express, { Response } from 'express';
+import errorHandler from './middlewares/error-handler';
+import { AppError } from './utils/error';
 
 const app = express();
+
+app.get('/', (req, res: Response) => {
+  res.status(200).send(`<h1>Welcome</h1>`);
+
+  // throw new AppError({
+  //   status: 400,
+  //   message: 'just bad',
+  //   code: 'bad_request',
+  // });
+});
+
+app.use(errorHandler);
 
 app.listen(config.port, () => {
   console.log(`server listening on port ${config.port}`);
